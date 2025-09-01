1941ef216d30420dd1b8cde67d800e27fe73083d
yabesh12 <samyabeshv@gmail.com>
2025-09-01 19:36:21 +0530

    initial-commit

--- app/main.py
+++ app/main.py
@@ -3,4 +3,8 @@ from app.api.v1 import api
 
 app = FastAPI(title="User Management Microservice")
 
+@app.get("/health")
+def health_check():
+    return {"status": "ok", "service": "User Management Microservice"}
+
 app.include_router(api.router, prefix="/v1", tags=["api"])
