822fda58d60c0c629e1809ec9105f4b3e35476e9
yabesh12 <samyabeshv@gmail.com>
2025-09-01 19:30:11 +0530

    first commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1 import api
 
-app = FastAPI(title="FastAPI Microservice")
+app = FastAPI(title="User Management Microservice")
 
 app.include_router(api.router, prefix="/v1", tags=["api"])
